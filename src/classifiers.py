fbb2fd3fdc319e42f0f054522341031b3ce83a60
Mauro Del Rio <mauro@crs4.it>
2020-07-10 15:04:31 +0200

    refactor classifier, add factory

--- src/classifiers.py
+++ src/classifiers.py
@@ -5,6 +5,7 @@ from commons import Slide
 import numpy as np
 from tifffile import imwrite
 from PIL import Image, ImageDraw, ImageFont
+import random
 
 
 class PatchFeature:
@@ -24,8 +25,7 @@ class FeatureTIFFRenderer(abc.ABC):
 
 def karolinska_rgb_convert(features: List[PatchFeature]) -> np.array:
     for feature in features:
-        cancer_percentage = feature.data[
-            KarolinskaDummyClassifier.Feature.CANCER_PERCENTAGE]
+        cancer_percentage = feature.data[KarolinskaFeature.CANCER_PERCENTAGE]
         mask_value = int(round(cancer_percentage * 255))
         data = (mask_value, 0, 0, 255) if cancer_percentage > 0 else (0, 0, 0,
                                                                       0)
@@ -34,8 +34,7 @@ def karolinska_rgb_convert(features: List[PatchFeature]) -> np.array:
 
 def karolinska_text_convert(features: List[PatchFeature]) -> np.array:
     for feature in features:
-        cancer_percentage = feature.data[
-            KarolinskaDummyClassifier.Feature.CANCER_PERCENTAGE]
+        cancer_percentage = feature.data[KarolinskaFeature.CANCER_PERCENTAGE]
         red = int(round(cancer_percentage * 255))
         txt = Image.new("RGBA", feature.patch.size, (0, 0, 0, 0))
         fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 30)
@@ -64,17 +63,45 @@ class BasicFeatureTIFFRenderer:
 
 
 class Classifier(abc.ABC):
+    @abc.abstractstaticmethod
+    def create(*args):
+        pass
+
     @abc.abstractmethod
     def classify_patch(patch: Patch) -> PatchFeature:
         pass
 
+    def classify(self,
+                 slide: Slide,
+                 patch_size: Tuple[int, int] = None) -> List[PatchFeature]:
+        features = []
+        for patch in slide.iterate_by_patch(patch_size):
+            features.append(self.classify_patch(patch))
+        return features
 
-class KarolinskaDummyClassifier(Classifier):
-    class Feature:
-        CANCER_PERCENTAGE = 'cancer_percentage'
 
+class KarolinskaFeature:
+    CANCER_PERCENTAGE = 'cancer_percentage'
+
+
+class KarolinskaRandomClassifier(Classifier):
+    @staticmethod
+    def create():
+        return KarolinskaRandomClassifier()
+
+    def classify_patch(self, patch: Patch) -> PatchFeature:
+        features = PatchFeature(
+            patch, {KarolinskaFeature.CANCER_PERCENTAGE: random.random()})
+        return features
+
+
+class KarolinskaTrueValueClassifier(Classifier):
     def __init__(self, mask: Slide):
         self.mask = mask
+
+    @staticmethod
+    def create(mask_filename):
+        return KarolinskaTrueValueClassifier(Slide(mask_filename))
 
     def classify_patch(self, patch: Patch) -> PatchFeature:
         image = self.mask.read_region(location=(patch.x, patch.y),
@@ -84,14 +111,7 @@ class KarolinskaDummyClassifier(Classifier):
         data = np.array(image.getchannel(0).getdata())
         features = PatchFeature(
             patch, {
-                KarolinskaDummyClassifier.Feature.CANCER_PERCENTAGE:
+                KarolinskaFeature.CANCER_PERCENTAGE:
                 sum(map(lambda el: 1 if el == 2 else 0, data)) / len(data)
             })
-        return features
-
-    def classify(self,
-                 patch_size: Tuple[int, int] = None) -> List[PatchFeature]:
-        features = []
-        for patch in self.mask.iterate_by_patch(patch_size):
-            features.append(self.classify_patch(patch))
         return features
--- src/runner.py
+++ src/runner.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from commons import Slide
-from classifiers import KarolinskaDummyClassifier,\
-    BasicFeatureTIFFRenderer, karolinska_text_convert,\
-    karolinska_rgb_convert
+import classifiers
 
 
-def classify_mask(in_filename, out_filename):
-    mask = Slide(in_filename)
+def classify_mask(in_filename, out_filename, classifier_name, *args):
+    slide = Slide(in_filename)
 
-    cl = KarolinskaDummyClassifier(mask)
+    cl = getattr(classifiers, classifier_name).create(*args)
 
-    features = cl.classify()
-    renderer = BasicFeatureTIFFRenderer(karolinska_rgb_convert,
-                                        mask.dimensions)
+    features = cl.classify(slide)
+    renderer = classifiers.BasicFeatureTIFFRenderer(
+        classifiers.karolinska_rgb_convert, slide.dimensions)
     print('rendering...')
     renderer.render(out_filename, features)
 
@@ -25,6 +23,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input')
     parser.add_argument('output')
+    parser.add_argument('-c',
+                        dest='classifier',
+                        default='KarolinskaTrueValueClassifier')
+    parser.add_argument('classifier_args', nargs='*')
+
     args = parser.parse_args()
 
-    classify_mask(args.input, args.output)
+    classify_mask(args.input, args.output, args.classifier,
+                  *args.classifier_args)
