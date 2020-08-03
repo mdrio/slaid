c4f405eb44d57a3f63f900da67346e9b62838759
Mauro Del Rio <mauro@crs4.it>
2020-08-03 15:22:51 +0200

    add extract tissue threshold params

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -6,6 +6,7 @@ import subprocess
 import unittest
 
 from slaid.commons import Slide
+from slaid.classifiers import TissueFeature
 
 DIR = os.path.dirname(os.path.realpath(__file__))
 input_ = os.path.join(DIR, 'data/PH10023-1.svs')
@@ -47,7 +48,7 @@ class GetTissueMaskTest(unittest.TestCase):
 
 
 class ExtractTissueTest(unittest.TestCase):
-    def test_extract_tissue(self):
+    def test_extract_tissue_default(self):
         subprocess.check_call(['extract_tissue.py', input_, output])
         with open(output, 'rb') as f:
             data = pickle.load(f)
@@ -55,10 +56,27 @@ class ExtractTissueTest(unittest.TestCase):
             self.assertTrue('extraction_level' in data)
             self.assertTrue('patch_size' in data)
             self.assertTrue('features' in data)
+            self.assertTrue(TissueFeature.TISSUE_MASK in data['features'])
 
             self.assertEqual(data['filename'], input_)
             self.assertEqual(data['patch_size'], (256, 256))
             self.assertEqual(data['extraction_level'], 2)
+
+    def test_extract_tissue_custom(self):
+        extr_level = 3
+        cmd = f'extract_tissue.py -l {extr_level} --no-mask -t 0.7 -T 0.09  {input_} {output}'
+        subprocess.check_call(cmd.split())
+        with open(output, 'rb') as f:
+            data = pickle.load(f)
+            self.assertTrue('filename' in data)
+            self.assertTrue('extraction_level' in data)
+            self.assertTrue('patch_size' in data)
+            self.assertTrue('features' in data)
+            self.assertTrue(TissueFeature.TISSUE_MASK not in data['features'])
+
+            self.assertEqual(data['filename'], input_)
+            self.assertEqual(data['patch_size'], (256, 256))
+            self.assertEqual(data['extraction_level'], extr_level)
 
 
 if __name__ == '__main__':
