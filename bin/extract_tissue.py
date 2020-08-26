134f0a44ea4b1d4f83aba1a9d31f21e60e052019
Mauro Del Rio <mauro@crs4.it>
2020-08-26 11:50:19 +0200

    removed get_tissue_mask

--- bin/extract_tissue.py
+++ bin/extract_tissue.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import pickle
+
 import pkg_resources
-from slaid.classifiers import BasicTissueClassifier, BasicTissueMaskPredictor
+
+from slaid.classifiers import (BasicTissueClassifier, BasicTissueMaskPredictor,
+                               get_tissue_mask)
 from slaid.commons import PATCH_SIZE
 from slaid.commons.ecvl import Slide
 from slaid.renderers import PickleRenderer
@@ -16,8 +20,8 @@ def main(slide_filename,
          minimum_tissue_ratio=0.1,
          include_mask=True,
          patch_size=PATCH_SIZE,
-         model_type='eddl',
-         gpu=False):
+         gpu=False,
+         only_mask=False):
     slide = Slide(slide_filename,
                   patch_size=patch_size,
                   extraction_level=extraction_level)
@@ -36,8 +40,19 @@ def main(slide_filename,
                                minimum_tissue_ratio=minimum_tissue_ratio,
                                include_mask_feature=include_mask)
 
-    pickle_renderer = PickleRenderer()
-    pickle_renderer.render(output_filename, slide)
+    if only_mask:
+        with open(output_filename, 'wb') as f:
+            pickle.dump(
+                {
+                    'filename': slide_filename,
+                    'dimensions': slide.dimensions,
+                    'extraction_level': extraction_level,
+                    'mask': get_tissue_mask(slide)
+                }, f)
+
+    else:
+        pickle_renderer = PickleRenderer()
+        pickle_renderer.render(output_filename, slide)
 
 
 if __name__ == '__main__':
@@ -79,10 +94,15 @@ if __name__ == '__main__':
                         default=False,
                         help="not include tissue mask",
                         action='store_true')
+    parser.add_argument('--only-mask',
+                        dest='only_mask',
+                        default=False,
+                        help="only tissue mask is returned",
+                        action='store_true')
     parser.add_argument('--gpu',
                         dest='gpu',
                         default=False,
-                        help="not include tissue mask",
+                        help="use gpu",
                         action='store_true')
 
     args = parser.parse_args()
@@ -90,4 +110,4 @@ if __name__ == '__main__':
         model = args.model
     main(args.slide, model, args.output, args.extraction_level,
          args.pixel_threshold, args.minimum_tissue_ratio, not args.no_mask,
-         args.patch_size)
+         args.patch_size, args.gpu, args.only_mask)
--- tests/test_bin.py
+++ tests/test_bin.py
@@ -14,51 +14,6 @@ output = '/tmp/output.pkl'
 slide = Slide(input_)
 
 
-class GetTissueMaskTest:
-    model = None
-
-    def test_get_tissue_mask_default_value(self):
-        subprocess.check_call(
-            ['get_tissue_mask.py', '-m', self.model, input_, output])
-        with open(output, 'rb') as f:
-            data = pickle.load(f)
-        self.assertTrue('filename' in data)
-        self.assertTrue('extraction_level' in data)
-        self.assertTrue('mask' in data)
-
-        self.assertEqual(data['filename'], input_)
-        self.assertEqual(data['extraction_level'], 2)
-        self.assertEqual(data['mask'].transpose().shape,
-                         slide.dimensions_at_extraction_level)
-        self.assertTrue(sum(sum(data['mask'])) > 0)
-
-    def test_get_tissue_mask_custom_value(self):
-        extr_level = 1
-        cmd = f'get_tissue_mask.py -m {self.model} -l {extr_level} -t 0.7 -T 0.09 {input_} {output}'
-        subprocess.check_call(cmd.split())
-        slide = Slide(input_, extraction_level=extr_level)
-        with open(output, 'rb') as f:
-            data = pickle.load(f)
-        self.assertTrue('filename' in data)
-        self.assertTrue('extraction_level' in data)
-        self.assertTrue('mask' in data)
-
-        self.assertEqual(data['filename'], input_)
-        self.assertEqual(data['dimensions'], slide.dimensions)
-        self.assertEqual(data['extraction_level'], extr_level)
-        self.assertEqual(data['mask'].transpose().shape,
-                         slide.dimensions_at_extraction_level)
-        self.assertTrue(sum(sum(data['mask'])) > 0)
-
-
-class SVMGetTissueMaskTest(GetTissueMaskTest, unittest.TestCase):
-    model = '../slaid/models/extract_tissue_LSVM-1.0.pickle'
-
-
-class EddlGetTissueMaskTest(GetTissueMaskTest, unittest.TestCase):
-    model = '../slaid/models/extract_tissue_eddl-1.0.0.bin'
-
-
 class ExtractTissueTest:
     model = None
 
@@ -92,6 +47,41 @@ class ExtractTissueTest:
             self.assertEqual(data['filename'], input_)
             self.assertEqual(data['patch_size'], (256, 256))
             self.assertEqual(data['extraction_level'], extr_level)
+
+    def test_get_tissue_mask_default_value(self):
+        subprocess.check_call([
+            'extract_tissue.py', '-m', self.model, '--only-mask', input_,
+            output
+        ])
+        with open(output, 'rb') as f:
+            data = pickle.load(f)
+        self.assertTrue('filename' in data)
+        self.assertTrue('extraction_level' in data)
+        self.assertTrue('mask' in data)
+
+        self.assertEqual(data['filename'], input_)
+        self.assertEqual(data['extraction_level'], 2)
+        self.assertEqual(data['mask'].transpose().shape,
+                         slide.dimensions_at_extraction_level)
+        self.assertTrue(sum(sum(data['mask'])) > 0)
+
+    def test_get_tissue_mask_custom_value(self):
+        extr_level = 1
+        cmd = f'extract_tissue.py --only-mask -m {self.model} -l {extr_level} -t 0.7 -T 0.09 {input_} {output}'
+        subprocess.check_call(cmd.split())
+        slide = Slide(input_, extraction_level=extr_level)
+        with open(output, 'rb') as f:
+            data = pickle.load(f)
+        self.assertTrue('filename' in data)
+        self.assertTrue('extraction_level' in data)
+        self.assertTrue('mask' in data)
+
+        self.assertEqual(data['filename'], input_)
+        self.assertEqual(data['dimensions'], slide.dimensions)
+        self.assertEqual(data['extraction_level'], extr_level)
+        self.assertEqual(data['mask'].transpose().shape,
+                         slide.dimensions_at_extraction_level)
+        self.assertTrue(sum(sum(data['mask'])) > 0)
 
 
 class SVMExtractTissueTest(ExtractTissueTest, unittest.TestCase):
