0112471534c298a1c57817e58cf6b6890aeff102
Mauro Del Rio <mauro@crs4.it>
2020-08-31 15:11:50 +0200

    update dockerfile and tests

--- tests/docker/test_docker.sh
+++ tests/docker/test_docker.sh
@@ -2,18 +2,25 @@
 set -xe
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-IMAGE='/data/test.tif'
+IMAGE='test.tif'
 OUTPUT=test-docker-output.pkl
 MODEL_DIR=$DIR/../../slaid/models
 rm -f $DIR/../data/$OUTPUT
 
 for model in $(ls $MODEL_DIR); do 
-  docker run  --rm -v $DIR/../data:/data  -v $MODEL_DIR:/models slaid extract_tissue.py -m /models/$model -l 0 $IMAGE /data/$OUTPUT
-  ls -l $DIR/../data/$OUTPUT
-  rm $DIR/../data/$OUTPUT
-  
-  docker run  --rm -v $DIR/../data:/data  -v $MODEL_DIR:/models slaid extract_tissue.py --only-mask -m /models/$model -l 0 $IMAGE /data/$OUTPUT
-  ls -l $DIR/../data/$OUTPUT
-  rm $DIR/../data/$OUTPUT
+  docker run  --rm -v $DIR/../data:/data  -v $MODEL_DIR:/models slaid classify.py -m /models/$model -l 0 /data/$IMAGE  -w pkl
+  ls -l $DIR/../data/${IMAGE}.output.pkl
+  rm $DIR/../data/${IMAGE}.output.pkl
 
+  docker run  --rm -v $DIR/../data:/data  -v $MODEL_DIR:/models slaid classify.py -m /models/$model -l 0 /data/$IMAGE
+  ls -l $DIR/../data/${IMAGE}.output.json
+  rm $DIR/../data/${IMAGE}.output.json
+  
+  docker run  --rm -v $DIR/../data:/data  -v $MODEL_DIR:/models slaid classify.py --only-mask -m /models/$model -l 0 /data/$IMAGE  -w pkl
+  ls -l $DIR/../data/${IMAGE}.output.pkl
+  rm $DIR/../data/${IMAGE}.output.pkl
+
+  docker run  --rm -v $DIR/../data:/data  -v $MODEL_DIR:/models slaid classify.py --only-mask -m /models/$model -l 0 /data/$IMAGE 
+  ls -l $DIR/../data/${IMAGE}.output.json
+  rm $DIR/../data/${IMAGE}.output.json
 done
