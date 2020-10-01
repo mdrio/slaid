2ef0981a2ef23e9aff64292b57cd95d686015aa9
Mauro Del Rio <mauro@crs4.it>
2020-10-01 17:47:40 +0200

    make eddl model pickable

--- slaid/models/eddl.py
+++ slaid/models/eddl.py
@@ -12,18 +12,9 @@ class Model(BaseModel):
         # Load the ANN tissue detector model implemented by using pyeddl
         # Create ANN topology (Same used during training phase)
 
-        def tissue_detector_DNN():
-            in_ = eddl.Input([3])
-
-            layer = in_
-            layer = eddl.ReLu(eddl.Dense(layer, 50))
-            layer = eddl.ReLu(eddl.Dense(layer, 50))
-            layer = eddl.ReLu(eddl.Dense(layer, 50))
-            out = eddl.Softmax(eddl.Dense(layer, 2))
-            net = eddl.Model([in_], [out])
-            return net
-
-        net = tissue_detector_DNN()
+        self._filename = filename
+        self._gpu = gpu
+        net = self._create_model(gpu)
         eddl.build(net, eddl.rmsprop(0.00001), ["soft_cross_entropy"],
                    ["categorical_accuracy"],
                    eddl.CS_GPU() if gpu else eddl.CS_CPU())
@@ -31,9 +22,17 @@ class Model(BaseModel):
         eddl.load(net, filename, "bin")
         self._model = net
 
-    def _predict(self, array: np.ndarray) -> List[Tensor]:
-        tensor = Tensor.fromarray(array)
-        return eddl.predict(self._model, [tensor])
+    @staticmethod
+    def _create_model(gpu: bool):
+        in_ = eddl.Input([3])
+
+        layer = in_
+        layer = eddl.ReLu(eddl.Dense(layer, 50))
+        layer = eddl.ReLu(eddl.Dense(layer, 50))
+        layer = eddl.ReLu(eddl.Dense(layer, 50))
+        out = eddl.Softmax(eddl.Dense(layer, 2))
+        net = eddl.Model([in_], [out])
+        return net
 
     def predict(self, array: np.ndarray) -> np.ndarray:
         predictions = self._predict(array)
@@ -44,3 +43,13 @@ class Model(BaseModel):
 
         flat_mask = np.vstack(temp_mask)
         return flat_mask
+
+    def _predict(self, array: np.ndarray) -> List[Tensor]:
+        tensor = Tensor.fromarray(array)
+        return eddl.predict(self._model, [tensor])
+
+    def __getstate__(self):
+        return dict(filename=self._filename, gpu=self._gpu)
+
+    def __setstate__(self, state):
+        self.__init__(**state)
