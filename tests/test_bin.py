cc6d0f8ff52e930f5bf4c0b9621567bde5215c7b
Mauro Del Rio <mauro@crs4.it>
2020-10-01 14:32:02 +0200

    remove obsolete test in test_bin

--- tests/test_bin.py
+++ tests/test_bin.py
@@ -109,26 +109,6 @@ class ExtractTissueTest:
             pickle.load(f)
         os.remove(f.name)
 
-    def test_input_as_pickle_with_filter(self):
-        slide = create_slide(input_)
-        slide.patches.add_feature('feature')
-        slide.patches.update_patch((0, 0), features={'feature': 1})
-        with NamedTemporaryFile(suffix='.pkl', delete=False) as f:
-            pickle.dump(slide, f)
-        extr_level = 1
-        cmd = f'classify.py -m {self.model} -f {self.feature} -l {extr_level} -F feature>=1 {f.name} {OUTPUT_DIR}'
-        subprocess.check_call(cmd.split())
-        output = os.path.join(
-            OUTPUT_DIR,
-            f'{os.path.splitext(os.path.basename(f.name))[0]}.{self.feature}.pkl'
-        )
-
-        with open(output, 'rb') as f_out:
-            slide_pickled = pickle.load(f_out)
-
-        self.assertTrue(slide_pickled.patches.dataframe.shape == (1, 2))
-        os.remove(f.name)
-
     def test_get_tissue_mask_default_value(self):
         subprocess.check_call([
             'classify.py', '-m', self.model, '-w', 'pkl', '-f', self.feature,
