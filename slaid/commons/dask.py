fa9af6472afdd18c6674bf3f4c8e0e965128337b
Mauro Del Rio <mauro@crs4.it>
2020-10-28 15:19:48 +0100

    update dask with downsample parameter

--- slaid/commons/dask.py
+++ slaid/commons/dask.py
@@ -1,13 +1,11 @@
 import logging
 
 import dask
-import dask.bag as db
 from dask.delayed import delayed
 from dask.distributed import Client
 from shapely.ops import cascaded_union
 
 from slaid.commons import Mask as BaseMask
-from slaid.commons import Polygon
 
 logger = logging.getLogger()
 
@@ -21,17 +19,20 @@ def init_client(*args, **kwargs):
 
 class Mask(BaseMask):
     @delayed
-    def _collect_polygons_from_batch(self, batch_idx, batch_size, threshold):
+    def _collect_polygons_from_batch(self, batch_idx, batch_size, threshold,
+                                     downsample):
         return super()._collect_polygons_from_batch(batch_idx, batch_size,
-                                                    threshold)
+                                                    threshold, downsample)
 
     @staticmethod
     def _cascaded_union(geom1, geom2):
         return cascaded_union([geom1, geom2])
 
-    def _get_merged_polygons(self, polygons):
-        bag = db.from_delayed(polygons)
-        multipolygon = bag.fold(self._cascaded_union).compute()
-        bag = db.from_sequence(list(multipolygon))
-        return bag.map(lambda p: (list(p.exterior.coords))).map(
-            Polygon).compute()
+    def _get_merged_polygons(self, polygons, downsample):
+        polygons = dask.compute(*polygons)
+        return super()._get_merged_polygons(polygons, downsample)
+        #  bag = db.from_delayed(polygons)
+        #  multipolygon = bag.fold(self._cascaded_union).compute()
+        #  bag = db.from_sequence(list(multipolygon))
+        #  return bag.map(lambda p: (list(p.exterior.coords))).map(
+        #      Polygon).compute()
