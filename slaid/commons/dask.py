1b9f97c9bf998d2e06be532fc159853827059fad
Mauro Del Rio <mauro@crs4.it>
2020-12-01 14:28:42 +0100

    add  to_zarr and to_tiledb on dask Mask implementation

--- slaid/commons/dask.py
+++ slaid/commons/dask.py
@@ -1,6 +1,10 @@
 import logging
+import os
 
+import dask
 import dask.array as da
+import tiledb
+import zarr
 from dask.distributed import Client
 
 from slaid.commons import Mask as BaseMask
@@ -13,7 +17,29 @@ def init_client(*args, **kwargs):
     return Client(*args, **kwargs)
 
 
+#  import dask
+#  dask.config.set(scheduler='synchronous')
+
+
 class Mask(BaseMask):
-    def to_tiledb(self, path, **kwargs):
-        da.to_tiledb(self.array, path)
+    def to_tiledb(self, path: str, overwrite: bool = False, **kwargs):
+        if overwrite:
+            try:
+                tiledb.remove(path)
+            except tiledb.libtiledb.TileDBError as ex:
+                logger.error(ex)
+        da.to_tiledb(self.array, path, compute=True, **kwargs)
         self._write_meta_tiledb(path)
+
+    #  FIXME: duplicate code
+    def to_zarr(self, path: str, overwrite: bool = False, **kwargs):
+        logger.info('dumping mask to zarr on path %s', path)
+        name = os.path.basename(path)
+        group = zarr.open_group(os.path.dirname(path))
+        if overwrite and name in group:
+            del group[name]
+        da.to_zarr(self.array, path, compute=True)
+        array = group[os.path.basename(path)]
+        for attr, value in self._get_attributes().items():
+            logger.info('writing attr %s %s', attr, value)
+            array.attrs[attr] = value
