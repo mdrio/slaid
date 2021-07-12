1f1cade8711babee365fff12da22e400f5b08833
Mauro Del Rio <mauro@crs4.it>
2021-07-12 10:34:06 +0200

    fix render mask

--- bin/render_mask.py
+++ bin/render_mask.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 from clize import run
+
 from slaid.writers import REGISTRY
 
 
@@ -11,7 +14,7 @@ def main(archive,
          downsample: ('d', int) = 1,
          threshold: (float, 't') = None):
 
-    slide = REGISTRY[archive.splitext()[1:]].load(archive)
+    slide = REGISTRY[os.path.splitext(archive)[1][1:]].load(archive)
     mask = slide.masks[mask_name]
     mask.to_image(downsample, threshold).save(output)
 
