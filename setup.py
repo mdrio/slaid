ec7e1f50ca858122a8f3b15bbc05c9240b82ab93
Mauro Del Rio <mauro@crs4.it>
2022-04-01 10:37:02 +0200

    add version using setuptools_scm

--- setup.py
+++ setup.py
@@ -6,16 +6,20 @@ with open('requirements.txt') as f:
     reqs = [line for line in f]
 reqs.append('pytest-spec')
 
-setuptools.setup(name="slaid",
-                 version=get_version(),
-                 description="AI for automatic analysis of slides",
-                 long_description_content_type="text/markdown",
-                 packages=setuptools.find_packages(),
-                 classifiers=[
-                     "Programming Language :: Python :: 3",
-                 ],
-                 python_requires='>=3.6',
-                 install_requires=reqs,
-                 scripts=['bin/classify.py', 'bin/annotate_onnx.py'],
-                 package_data={'': ['resources/models/*']},
-                 include_package_data=True)
+setuptools.setup(
+    name="slaid",
+    version=get_version(),
+    description="AI for automatic analysis of slides",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+    install_requires=reqs,
+    scripts=['bin/classify.py', 'bin/annotate_onnx.py'],
+    package_data={'': ['resources/models/*']},
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+)
